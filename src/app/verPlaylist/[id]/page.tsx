418d089bc9852e09b5eac3f1fd5092ec3260570d
Matheus07Henrique <matheushenriquefs07@gmail.com>
2023-07-28 00:20:39 -0300

    playlist view page

--- src/app/verPlaylist/[id]/page.tsx
+++ src/app/verPlaylist/[id]/page.tsx
@@ -1,10 +1,21 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { PlaylistData } from "../../../components/PlaylistData";
 
-export default async function verPlaylist() {
+export default function VerPlaylist() {
+    const [id, setId] = useState('');
+
+    useEffect(() => {
+        const pathParts = window.location.pathname.split('/');
+        const playlistId = pathParts[pathParts.length - 1];
+
+        setId(playlistId);
+    }, []);
 
     return (
         <div>
-            <PlaylistData/>
+            <PlaylistData id={id} />
         </div>
     );
 }
